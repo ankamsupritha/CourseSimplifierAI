4a6c7169776b0e9db505e92d27ab819382d60464
supritha_ankam <ankamsupritha10@gmail.com>
2026-07-15 02:55:05 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -442,6 +442,10 @@ def logout():
     return redirect("/")
 
 
-if __name__ == "__main__":
+import os
 
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
